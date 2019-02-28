b779175794423ee08e761350c98803e03379e478
umar <f14b150@ibitpu.edu.pk>
2019-02-28 21:37:11 +0500

    Updates

---
+++ precache-manifest.5583f6c5e1610fc8e122b8253b6e411d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a0d8234b60ba3a9126e",
+    "url": "/FaceRecognitionApp/static/js/runtime~main.1a0d8234.js"
+  },
+  {
+    "revision": "c53e7b0dd6aa2be46a77",
+    "url": "/FaceRecognitionApp/static/js/main.c53e7b0d.chunk.js"
+  },
+  {
+    "revision": "782cce84073a9ef844a0",
+    "url": "/FaceRecognitionApp/static/js/2.782cce84.chunk.js"
+  },
+  {
+    "revision": "c53e7b0dd6aa2be46a77",
+    "url": "/FaceRecognitionApp/static/css/main.0d5bfa92.chunk.css"
+  },
+  {
+    "revision": "782cce84073a9ef844a0",
+    "url": "/FaceRecognitionApp/static/css/2.8fef475e.chunk.css"
+  },
+  {
+    "revision": "97886b0c655fd4faf3d0703afafb7d0c",
+    "url": "/FaceRecognitionApp/index.html"
+  }
+];
